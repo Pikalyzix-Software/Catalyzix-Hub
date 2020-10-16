b8842d9d54169a2262ac99c755dbc5fa71679815
Vault <62789852+VaultGitos@users.noreply.github.com>
2020-10-16 21:57:36 +0700

    Create PikalyzixHub.lua

---
+++ PikalyzixHub.lua
@@ -0,0 +1,26 @@
+--[[
+  ____  _ _         _           _        _   _       _     
+ |  _ \(_) | ____ _| |_   _ ___(_)_  __ | | | |_   _| |__  
+ | |_) | | |/ / _` | | | | |_  / \ \/ / | |_| | | | | '_ \ 
+ |  __/| |   < (_| | | |_| |/ /| |>  <  |  _  | |_| | |_) |
+ |_|   |_|_|\_\__,_|_|\__, /___|_/_/\_\ |_| |_|\__,_|_.__/ 
+                      |___/
+                      Copy Right by Pikalyzix Hub
+                      Credits : {
+                      722485792076988586 - Pikalyzix#2012
+                      757148941346144256 - Vault ilmo#0001
+]]
+
+if game.PlaceId == 1401417393 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollSystemTest.lua'),true))()
+elseif game.PlaceId == 4169490976 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua'),true))()
+elseif game.PlaceId == 2686500207 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/ABD.lua'),true))()
+elseif game.PlaceId == 23578803 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/HOTELELEPHANT.lua'),true))()
+elseif game.PlaceId == 2041312716 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollEngine.lua'),true))()
+elseif game.PlaceId == 71315343 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/DBR.lua'),true))()
+end
