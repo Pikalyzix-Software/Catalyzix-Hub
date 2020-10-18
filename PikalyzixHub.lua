c02e433d2cb450040acdbf49f1bd6da55e248754
Vault <62789852+VaultGitos@users.noreply.github.com>
2020-10-18 18:40:27 +0700

    Update PikalyzixHub.lua

--- PikalyzixHub.lua
+++ PikalyzixHub.lua
@@ -11,25 +11,50 @@
                       757148941346144256 - Vaultary#0001
 ]]
 
+local PikalyzixHub = library:StartWindow()
+
+
 if game.PlaceId == 1401417393 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollSystemTest.lua'),true))()
+  PikalyzixHub:CreateButton("Ragdoll System Test", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollSystemTest.lua'),true))()
+  end)  
 elseif game.PlaceId == 4169490976 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua'),true))()
+  PikalyzixHub:CreateButton("Mortem Metallum", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua'),true))()
+  end)
 elseif game.PlaceId == 2686500207 then
+  PikalyzixHub:CreateButton("Mortem Metallum", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua'),true))()
+  end)
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/ABD.lua'),true))()
 elseif game.PlaceId == 23578803 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/HOTELELEPHANT.lua'),true))()
+  PikalyzixHub:CreateButton("Hotel Elephant", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/HOTELELEPHANT.lua'),true))()
+  end) 
 elseif game.PlaceId == 2041312716 then
+  PikalyzixHub:CreateButton("Mortem Metallum", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua'),true))()
+  end)
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollEngine.lua'),true))()
 elseif game.PlaceId == 71315343 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/DBR.lua'),true))()
+  PikalyzixHub:CreateButton("Dragon Ball Rage", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/DBR.lua'),true))()
+  end)
 elseif game.PlaceId == 537413528 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/BuildABoat.lua'),true))()
+  PikalyzixHub:CreateButton("Build A Boat For Treasure", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/BuildABoat.lua'),true))()
+  end)
 elseif game.PlaceId == 189707 then
-  loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/NaturalDisasterSurvival.lua'),true))()
+  PikalyzixHub:CreateButton("Natural Disaster Survival", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/NaturalDisasterSurvival.lua'),true))()
+  end)
 elseif game.PlaceId == 318978013 then
-  loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/KickOff.lua'),true))()
+  PikalyzixHub:CreateButton("Kick Off", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/KickOff.lua'),true))()
+  end)
 elseif game.PlaceId == 155615604 then
-  loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/PL.lua'),true))()
+  PikalyzixHub:CreateButton("Prison Life", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/PL.lua'),true))()
+  end)
 end
 
