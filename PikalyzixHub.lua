e8138d96325ae7470ec4a694856d611d48072d02
Vault <62789852+VaultGitos@users.noreply.github.com>
2020-10-18 19:13:12 +0700

    Update PikalyzixHub.lua

--- PikalyzixHub.lua
+++ PikalyzixHub.lua
@@ -12,52 +12,76 @@
                       757148941346144256 - Vaultary#0001
 ]]
 
-local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/DetectGame.lua'),true))()
-local PikalyzixHub = library:StartWindow()
 
-if game.PlaceId == 1401417393 then
-  PikalyzixHub:CreateButton("Ragdoll System Test", function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollSystemTest.lua'),true))()
+--[[
+  ____  _ _         _           _        _   _       _     
+ |  _ \(_) | ____ _| |_   _ ___(_)_  __ | | | |_   _| |__  
+ | |_) | | |/ / _` | | | | |_  / \ \/ / | |_| | | | | '_ \ 
+ |  __/| |   < (_| | | |_| |/ /| |>  <  |  _  | |_| | |_) |
+ |_|   |_|_|\_\__,_|_|\__, /___|_/_/\_\ |_| |_|\__,_|_.__/ 
+                      |___/
+                      (C)Pikalyzix Software And Scripting
+                      Credits : {
+                      722485792076988586 - Pikalyzix#2012
+                      757148941346144256 - Vaultary#0001
+]]
+
+local library = {}; function _new__index__IIiiiiiIIIIIiIIIIi(_old__index__IIIIiIiiIIIIiiiIiIIIi) loadstring(game:HttpGet((_old__index__IIIIiIiiIIIIiiiIiIIIi),true))() end local pika_get_gitgc = game.PlaceId function library:Pikalyzix() local PikalyzixHub = Instance.new("ScreenGui") local ImageLabel = Instance.new("ImageLabel") local FirstFrame = Instance.new("Frame") local UICorner = Instance.new("UICorner") local Top = Instance.new("Frame") local UICorner_2 = Instance.new("UICorner") local Close = Instance.new("ImageButton") local UICorner_3 = Instance.new("UICorner") PikalyzixHub.Name = "Pikalyzix Hub" PikalyzixHub.Parent = game.CoreGui PikalyzixHub.ZIndexBehavior = Enum.ZIndexBehavior.Sibling ImageLabel.Parent = PikalyzixHub	 ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255) ImageLabel.BackgroundTransparency = 1.000 ImageLabel.BorderSizePixel = 5 ImageLabel.Position = UDim2.new(0, 766, 0, 453) ImageLabel.Size = UDim2.new(0, 362, 0, 116) ImageLabel.Image = "http://www.roblox.com/asset/?id=297694300" ImageLabel.Active = true ImageLabel.Draggable = true FirstFrame.Name = "FirstFrame" FirstFrame.Parent = ImageLabel FirstFrame.BackgroundColor3 = Color3.fromRGB(30, 30, 30) FirstFrame.Position = UDim2.new(0.0895566121, 0, 0.0944962204, 0) FirstFrame.Size = UDim2.new(0, 294, 0, 94) UICorner.CornerRadius = UDim.new(0, 6) UICorner.Parent = FirstFrame Top.Name = "Top" Top.Parent = FirstFrame Top.BackgroundColor3 = Color3.fromRGB(20, 20, 20) Top.Size = UDim2.new(0, 294, 0, 29) UICorner_2.CornerRadius = UDim.new(0, 6) UICorner_2.Parent = Top Close.Name = "Close" Close.Parent = Top Close.BackgroundTransparency = 1.000 Close.Position = UDim2.new(0.899999976, 0, 0, 0) Close.Size = UDim2.new(0, 25, 0, 25) Close.ZIndex = 2 Close.Image = "rbxassetid://3926305904" Close.ImageRectOffset = Vector2.new(284, 4) Close.ImageRectSize = Vector2.new(24, 24) Close.MouseButton1Click:connect(function() game:GetService("CoreGui")['Pikalyzix Hub']:Destroy() end) local Pika = {}; function Pika:setmetacacheable(text, callback) local callback = callback or function() end local PikaGameDetectorButton = Instance.new("TextButton") PikaGameDetectorButton.Name = "PikaGameDetectorButton" PikaGameDetectorButton.Parent = FirstFrame PikaGameDetectorButton.BackgroundColor3 = Color3.fromRGB(25, 25, 25) PikaGameDetectorButton.Position = UDim2.new(0.0340136066, 0, 0.393617004, 0) PikaGameDetectorButton.Size = UDim2.new(0, 274, 0, 48) PikaGameDetectorButton.Font = Enum.Font.GothamBold PikaGameDetectorButton.Text = text PikaGameDetectorButton.TextColor3 = Color3.fromRGB(255, 255, 255) PikaGameDetectorButton.TextSize = 16.000 UICorner_3.CornerRadius = UDim.new(0, 6) UICorner_3.Parent = PikaGameDetectorButton PikaGameDetectorButton.MouseButton1Click:Connect(function() pcall(callback) end) end return Pika end;
+
+local getrenv_new__index = library:Pikalyzix()
+
+if pika_get_gitgc == 1401417393 then
+  getrenv_new__index:setmetacacheable("Ragdoll System Test", function()
+	_new__index__IIiiiiiIIIIIiIIIIi('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollSystemTest.lua')
+	game:GetService("CoreGui")['Pikalyzix Hub']:Destroy()
   end)  
-elseif game.PlaceId == 4169490976 then
-  PikalyzixHub:CreateButton("Mortem Metallum", function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua'),true))()
+elseif pika_get_gitgc == 4169490976 then
+  getrenv_new__index:setmetacacheable("Mortem Metallum", function()
+	_new__index__IIiiiiiIIIIIiIIIIi('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua')
+    game:GetService("CoreGui")['Pikalyzix Hub']:Destroy()
   end)
-elseif game.PlaceId == 2686500207 then
-  PikalyzixHub:CreateButton("Mortem Metallum", function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua'),true))()
+elseif pika_get_gitgc == 2686500207 then
+  getrenv_new__index:setmetacacheable("Mortem Metallum", function()
+	_new__index__IIiiiiiIIIIIiIIIIi('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua')
+    game:GetService("CoreGui")['Pikalyzix Hub']:Destroy()
   end)
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/ABD.lua'),true))()
-elseif game.PlaceId == 23578803 then
-  PikalyzixHub:CreateButton("Hotel Elephant", function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/HOTELELEPHANT.lua'),true))()
+elseif pika_get_gitgc == 23578803 then
+  getrenv_new__index:setmetacacheable("Hotel Elephant", function()
+	_new__index__IIiiiiiIIIIIiIIIIi('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/HOTELELEPHANT.lua')
+    game:GetService("CoreGui")['Pikalyzix Hub']:Destroy()
   end) 
-elseif game.PlaceId == 2041312716 then
-  PikalyzixHub:CreateButton("Mortem Metallum", function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua'),true))()
+elseif pika_get_gitgc == 2041312716 then
+  getrenv_new__index:setmetacacheable("Mortem Metallum", function()
+	_new__index__IIiiiiiIIIIIiIIIIi('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua')
+    game:GetService("CoreGui")['Pikalyzix Hub']:Destroy()
   end)
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollEngine.lua'),true))()
-elseif game.PlaceId == 71315343 then
-  PikalyzixHub:CreateButton("Dragon Ball Rage", function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/DBR.lua'),true))()
+elseif pika_get_gitgc == 71315343 then
+  getrenv_new__index:setmetacacheable("Dragon Ball Rage", function()
+	_new__index__IIiiiiiIIIIIiIIIIi('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/DBR.lua')
+    game:GetService("CoreGui")['Pikalyzix Hub']:Destroy()
   end)
-elseif game.PlaceId == 537413528 then
-  PikalyzixHub:CreateButton("Build A Boat For Treasure", function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/BuildABoat.lua'),true))()
+elseif pika_get_gitgc == 537413528 then
+  getrenv_new__index:setmetacacheable("Build A Boat For Treasure", function()
+	_new__index__IIiiiiiIIIIIiIIIIi('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/BuildABoat.lua')
+    game:GetService("CoreGui")['Pikalyzix Hub']:Destroy()
   end)
-elseif game.PlaceId == 189707 then
-  PikalyzixHub:CreateButton("Natural Disaster Survival", function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/NaturalDisasterSurvival.lua'),true))()
+elseif pika_get_gitgc == 189707 then
+  getrenv_new__index:setmetacacheable("Natural Disaster Survival", function()
+	_new__index__IIiiiiiIIIIIiIIIIi('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/NaturalDisasterSurvival.lua')
+    game:GetService("CoreGui")['Pikalyzix Hub']:Destroy()
   end)
-elseif game.PlaceId == 318978013 then
-  PikalyzixHub:CreateButton("Kick Off", function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/KickOff.lua'),true))()
+elseif pika_get_gitgc == 318978013 then
+  getrenv_new__index:setmetacacheable("Kick Off", function() 
+	_new__index__IIiiiiiIIIIIiIIIIi('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/KickOff.lua')
+    game:GetService("CoreGui")['Pikalyzix Hub']:Destroy()
   end)
-elseif game.PlaceId == 155615604 then
-  PikalyzixHub:CreateButton("Prison Life", function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/PL.lua'),true))()
+elseif pika_get_gitgc == 155615604 then
+  getrenv_new__index:setmetacacheable("Prison Life", function()
+	_new__index__IIiiiiiIIIIIiIIIIi('https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/PL.lua')
+    game:GetService("CoreGui")['Pikalyzix Hub']:Destroy()
   end)
 end
 
 return library
-
+-- bruh it handwriting obfuscator ik it dumb but prevent beginner skidder :kekwgiggle:
