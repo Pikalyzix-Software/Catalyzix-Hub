497f688e9186ed9339f260fd1e29a32db1558f03
Pikalyzix <62042556+Pikalyzix@users.noreply.github.com>
2020-10-19 19:13:21 -0400

    Update PikalyzixHub.lua

--- PikalyzixHub.lua
+++ PikalyzixHub.lua
@@ -12,30 +12,30 @@
 --]]
 
 if game.PlaceID == 1401417393 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollSystemTest.lua"))() 
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollSystemTest.lua"))() 
 elseif game.PlaceID == 4169490976 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua"))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/MortemMetallum.lua"))()
 elseif game.PlaceID == 2686500207 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/ABD.lua"))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/ABD.lua"))()
 elseif game.PlaceID == 23578803 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/HOTELELEPHANT.lua"))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/HOTELELEPHANT.lua"))()
 elseif game.PlaceID == 2041312716 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollEngine.lua"))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/RagdollEngine.lua"))()
 elseif game.PlaceID == 71315343 or 1357512648 or 3371469539 or 3336119605 or 3422324662 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/DBR.lua"))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/DBR.lua"))()
 elseif game.PlaceID == 537413528 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/BuildABoat.lua"))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/BuildABoat.lua"))()
 elseif game.PlaceID == 189707 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/NaturalDisasterSurvival.lua"))()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/NaturalDisasterSurvival.lua"))()
 elseif game.PlaceID == 318978013 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/KickOff.lua"))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/KickOff.lua"))()
 elseif game.PlaceID == 155615604 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/PL.lua"))()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/PL.lua"))()
 elseif game.PlaceID == 855499080 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/Skywar.lua"))()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Pikalyzix-Software/Pikalyzix-Software-Release/main/Skywar.lua"))()
 else
     StarterGui:SetCore("SendNotification", {
         Title = "Pikalyzix Hub | ERROR";
-        Text = "This game is not supported."
+        Text = "This game is not supported yet!"
     })
 end
