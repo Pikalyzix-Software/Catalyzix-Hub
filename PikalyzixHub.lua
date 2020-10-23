bda1b9e350631766252440b2a0c93fb5bc86e3cc
Vault <62789852+VaultGitos@users.noreply.github.com>
2020-10-23 09:56:48 +0700

    Update PikalyzixHub.lua

--- PikalyzixHub.lua
+++ PikalyzixHub.lua
@@ -10,6 +10,96 @@
                       722485792076988586 - Pikalyzix#2012
                       757148941346144256 - Vaultary#0001
 --]]
+-- Direct X 11 ESP --
+local function callback(text)
+    if text == "Execute" then
+local OwlESP = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/VzltexArch/Scripts/master/ESPFuctions.txt"))();
+
+local players = game:GetService("Players");
+local runService = game:GetService("RunService");
+local localPlayer = players.LocalPlayer;
+local tracking = {};
+
+local remove = table.remove;
+local fromRGB = Color3.fromRGB;
+
+local espColor = fromRGB(255, 255, 255);
+local teamCheck = false;
+
+local function characterRemoving(char)
+    for i, v in next, tracking do
+        if v.char == char then
+            v:remove();
+            remove(tracking, i);
+        end;
+    end;
+end;
+
+local function characterAdded(plr)
+    local char = plr.Character;
+    char:WaitForChild("HumanoidRootPart"); char:WaitForChild("Head");
+    tracking[#tracking + 1] = OwlESP.new({
+        plr = plr,
+        espBoxVisible = true,
+        tracerVisible = true,
+        text = plr.Name,
+        teamCheck = teamCheck,
+        espColor = espColor
+    });
+end;
+
+for i, v in next, players:GetPlayers() do
+    if v ~= localPlayer then
+        local char = v.Character;
+        if char and char:FindFirstChild("HumanoidRootPart") and char:FindFirstChild("Head") then
+            tracking[#tracking + 1] = OwlESP.new({
+                plr = v,
+                espBoxVisible = true,
+                tracerVisible = true,
+                text = v.Name,
+                teamCheck = teamCheck,
+                espColor = espColor
+            });
+        end;
+        v.CharacterAdded:Connect(function()
+            characterAdded(v);
+        end);
+        v.CharacterRemoving:Connect(characterRemoving);
+    end;
+end;
+
+local function playerAdded(plr)
+    plr.CharacterAdded:Connect(function()
+        characterAdded(plr);
+    end);
+    plr.CharacterRemoving:Connect(characterRemoving);
+end;
+
+players.PlayerAdded:Connect(playerAdded);
+
+runService.RenderStepped:Connect(function()
+    for i, v in next, tracking do
+        v:update();
+    end;
+end);
+    elseif text == "Cancel" then
+        return
+    end
+end
+
+local bindableFunction = Instance.new("BindableFunction")
+bindableFunction.OnInvoke = callback
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Direct X11 ESP";
+    Text = "Catalyzix Hub";
+    Icon = "";
+    Duration = 40;
+    Callback = bindableFunction;
+    Button1 = "Execute";
+    Button2 = "Cancel";
+})
+-- Game Detector + Exlusive --
 if syn and syn.cache_replace and syn.cache_invalidate and syn.is_cached and syn.write_clipboard and syn.set_thread_identity then
 game.StarterGui:SetCore("SendNotification", {
         Title = "Pikalyzix Hub | Exclusive";
