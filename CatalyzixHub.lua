584dae7ee40182e311fde19f3a6da4f0df2a07e6
Pikalyzix <62042556+Pikalyzix@users.noreply.github.com>
2020-10-28 23:02:11 -0400

    Update CatalyzixHub.lua

--- CatalyzixHub.lua
+++ CatalyzixHub.lua
@@ -14,7 +14,7 @@
 		      722485792076988586 - Catalyzix#2012
                       757148941346144256 - Vaultary#0001
 
---]]
+]]
 if game:FindFirstChild("CoreGui") then
 	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pikalyzix-Software/Catalyzix-Hub/main/DetectGame.lua'),true))()
 end
